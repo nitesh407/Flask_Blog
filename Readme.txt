30c48adc0293d0f1e608ad3ca0c1d281810a7ef6
nitesh407 <snitesh407@gmail.com>
2019-06-11 15:22:21 +0530

    Til now, done with the templates

--- Readme.txt
+++ Readme.txt
@@ -13,3 +13,15 @@
 	
 		if __name__ == '__main__':
 			app.run(debug=True)
+
+
+- TO import the templates we use render_template() function.
+- We are using a variable posts to access the content of this page list(posts) to the template page.
+
+- Now we gonna use template inheritance, because there is alot of simiar code in all the templates( so we created a htmlpage, layout.html)
+
+- We gonna create a block, so the block is a section thaf child section can override.
+
+- In flask we out css and javascript files in a static directory just because all these are static files.
+
+- To include the static files we use a function named url_for(), is a function that will find the exect location of the routes.
--- flask_blog.py
+++ flask_blog.py
@@ -1,15 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__) #app variable here is the object of Flask class, __name__ is simply a name of the module used to locate the other templates of stuffs.
 #routes are what we type browsers to go to different pages.Called decorators.They are just to add aditional functionalities to the existing function.s
+
+
+posts = [
+	{
+		'author':'Nitesh Sharma',
+		'title': 'First bolg',
+		'content': 'First blog content',
+		'date_posted' : 'October 23, 2019'
+	},
+	{
+		'author':'Shiva reddy',
+		'title': 'Second blog',
+		'content': 'Second blog content',
+		'date_posted' : 'October 30, 2019'
+	}
+
+
+]
+
 
 @app.route("/") #Home page 
 @app.route("/home")
 def home():
-    return "<h1>Hello Nitesh</h1>" 
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-	return "<h1>About page</h1>"
+	return render_template('about.html', title='About')
 
 if __name__ == '__main__' :
 	app.run(debug=True) 
